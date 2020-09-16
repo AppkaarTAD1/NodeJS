3a843978bc0796edcf3efbe8bcd329deca0bdca4
Nima <70260876+nimajvnm@users.noreply.github.com>
2020-09-16 17:07:58 +0430

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const App = express();
-App.set('vash engine','vash')
-App.get('/',(req,res)=> {
-    res.render('index.vash',{name:'Nima'});
-})
-App.listen(6969);
-console.log('Server Is Running');
